072181bcb1ff604f7b1d6beb90bab9533dc869d3
ratneshgujarathi <ratneshgujarathi@outlook.com>
2025-06-20 09:35:56 +0530

    linked list

---
+++ linked list/split_ll_k_parts.py
@@ -0,0 +1,53 @@
+class ListNode:
+    def __init__(self, val, next = None):
+        self.val = val
+        self.next = next
+
+    @staticmethod
+    def print_linked_list(head):
+        curr = head
+        while curr:
+            print(curr.val, end=" -> ")
+            curr = curr.next
+        print("None")
+
+
+class Solution:
+    def find_tail(self, head, k):
+        k-=1
+        while head and k > 0:
+            k-=1
+            head = head.next
+        return head
+    
+    def get_length(self, head):
+        count = 0
+        while head:
+            count+=1
+            head = head.next
+        return count
+    
+    def split_ll(self, head, k):
+        ans = []
+        n = self.get_length(head)
+        part_size = n // k
+        extra = n % k
+        temp = head
+
+        for i in range(k):
+            curr_part_size = part_size + (1 if i < extra else 0 )
+            if curr_part_size == 0:
+                ans.append(None)
+                continue
+
+            part_head = temp
+
+            tail = self.find_tail(part_head, curr_part_size)
+
+            if tail:
+                temp = tail.next
+                tail.next = None
+
+            ans.append(part_head)
+
+        return ans
